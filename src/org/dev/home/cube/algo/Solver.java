b1d7a5d780ca510e23ea0ce118ce7cd21226c587
dave-bt <beattie_dave@hotmail.com>
2015-12-11 22:33:24 +0000

    solves cross on first layer

--- src/org/dev/home/cube/algo/Solver.java
+++ src/org/dev/home/cube/algo/Solver.java
@@ -49,7 +49,7 @@ public class Solver {
 		cross_pieces.add(cube.findPiece(new Position(new Coords3D(1, 1, 0), new Colours3D(Colour.Blue, Colour.White, null))));
 		cross_pieces.add(cube.findPiece(new Position(new Coords3D(-1, 1, 0), new Colours3D(Colour.Green, Colour.White, null))));
 
-		//try and get one piece in position to start with
+		//try and get one piece in position to start with. TODO might turn 3 times for nothing here!!
 		int rotations=0;
 		while (countHomes(cross_pieces) == 0 && rotations<3)
 		{
@@ -57,36 +57,124 @@ public class Solver {
 			rotations++;
 		}	
 		
-		//iterate through the four pieces choosing appropriate algo based on where it is 
+		//iterate through the four pieces choosing appropriate algo based on which layer it is in 
 		for (Piece piece : cross_pieces)
 		{
 			Coords3D location_current = piece.getCurrentPosition().getLocation();
 			Coords3D location_home = piece.getHomePosition().getLocation();
 			Colours3D colours = piece.getCurrentPosition().getColours();
-			switch (location_current.y)
+			if (!piece.isHome())
 			{
-			case 1:
-				switch (colours.getX())
+				switch (location_current.y)
 				{
-				case White :
-					if (location_current.equals(location_home))
+				case 1:					
+					//turn piece to middle layer
+					if (location_current.x!=0)
 					{
-						break;
+						cube.rotate(new Plane(Axis.X, location_current.x), Angle.Ninety);
 					}
 					else
 					{
-						//piece is oriented correctly but not in right place on top layer
-						
+						cube.rotate(new Plane(Axis.Z, location_current.z), Angle.Ninety);
 					}
-				default:
+					solveTopLayerCross_MiddleLayerPiece(piece);
 					break;					
-				}
-				break;
+				case 0:
+					solveTopLayerCross_MiddleLayerPiece(piece);
+					break;
+				case -1:
+					//rotate until lined up directly underneath home position
+					while (location_current.x!=location_home.x || location_current.z!=location_home.z)
+					{
+						cube.rotate(new Plane(Axis.Y, -1), Angle.Ninety);
+					}
+					if (colours.getY()==Colour.White)
+					{
+						//turn piece to top layer
+						if (location_current.x!=0)
+						{
+							cube.rotate(new Plane(Axis.X, location_current.x), Angle.OneEighty);
+						}
+						else
+						{
+							cube.rotate(new Plane(Axis.Z, location_current.z), Angle.OneEighty);
+						}
+					}
+					else
+					{
+						//rotate to middle layer 
+						if (location_current.x!=0)
+						{
+							cube.rotate(new Plane(Axis.X, location_current.x), Angle.MinusNinety);
+						}
+						else
+						{
+							cube.rotate(new Plane(Axis.Z, location_current.z), Angle.MinusNinety);
+						}
+						solveTopLayerCross_MiddleLayerPiece(piece);
+					}
+					break;
+				}		
 			}
 		}
-		
 	}
 	
+	private void solveTopLayerCross_MiddleLayerPiece(Piece piece)
+	{
+		Coords3D location_current = piece.getCurrentPosition().getLocation();
+		Coords3D location_home = piece.getHomePosition().getLocation();
+		Colours3D colours_current = piece.getCurrentPosition().getColours();
+		
+		if(location_home.x!=0)
+		{
+			//rotate piece until it is in the correct z plane and has white on
+			//it's z face
+			while (location_current.x!=location_home.x || colours_current.getZ()!=Colour.White)
+			{
+				cube.rotate(new Plane(Axis.Y, 0), Angle.Ninety);
+			}
+			//turn up to top layer
+			if (location_current.z==1)
+			{
+				cube.rotate(new Plane(Axis.X, location_current.x), Angle.Ninety);
+			}
+			else
+			{
+				cube.rotate(new Plane(Axis.X, location_current.x), Angle.MinusNinety);
+			}
+		}
+		else
+		{
+			//rotate piece until it is in the correct x plane and has white on
+			//it's x face
+			while (location_current.z!=location_home.z || colours_current.getX()!=Colour.White)
+			{
+				cube.rotate(new Plane(Axis.Y, 0), Angle.Ninety);
+			}
+			//turn up to top layer
+			if (location_current.x==1)
+			{
+				cube.rotate(new Plane(Axis.Z, location_current.z), Angle.MinusNinety);
+			}
+			else
+			{
+				cube.rotate(new Plane(Axis.Z, location_current.z), Angle.Ninety);
+			}
+		}
+ 	}
+	
+	/* gets the 
+	private Plane getNonZeroOrthogonalPlane(Piece piece, Plane plane)
+	{
+		if (location_current.x!=0)
+		{
+			new Plane(Axis.X, location_current.x);
+		}
+		else
+		{
+			cube.rotate(new Plane(Axis.Y, location_current.y), Angle.Ninety);
+		}
+	}*/
 	
 	
 	
